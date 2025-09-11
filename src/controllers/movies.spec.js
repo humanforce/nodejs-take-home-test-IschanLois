4ffe652dd66d4b98050fe73614532a881ad5726c
Christian Olo <christianlois.olo@gmail.com>
2025-09-12 06:51:42 +0800

    chore: setup jest

---
+++ src/controllers/movies.spec.js
@@ -0,0 +1,9 @@
+import { describe, expect } from '@jest/globals'
+
+describe('movies controller', () => {
+  describe('createMovie()', () => {
+    it('should create a new movie', async () => {
+      expect(1).toBe(1)
+    })
+  })
+})

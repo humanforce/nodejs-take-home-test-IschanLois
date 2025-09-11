463f97635b2283bddf205fcf68b23ce6175b0e31
Christian Olo <christianlois.olo@gmail.com>
2025-09-12 07:29:30 +0800

    test: add unit tests for createMovie() handler

--- src/controllers/movies.spec.js
+++ src/controllers/movies.spec.js
@@ -1,9 +1,63 @@
-import { describe, expect } from '@jest/globals'
+import { beforeEach, describe, jest } from '@jest/globals'
+
+import { createMovie } from './movies'
+import { Movie } from '../models'
 
 describe('movies controller', () => {
+  let res = null
+  let next = null
+
+  const params = { movieId: 1 }
+  const body = {
+    title: 'Inception',
+    director: 'Christopher Nolan',
+    release_year: 2010,
+  }
+
+  beforeEach(() => {
+    jest.resetAllMocks()
+    jest.clearAllMocks()
+
+    next = jest.fn()
+    res = {
+      sendStatus: jest.fn(),
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    }
+  })
+
   describe('createMovie()', () => {
     it('should create a new movie', async () => {
-      expect(1).toBe(1)
+      const req = { params, body }
+
+      const mockMovie = {
+        movie_id: 1,
+        title: 'Inception',
+        director: 'Christopher Nolan',
+        release_year: 2010,
+      }
+
+      jest.spyOn(Movie, 'create').mockResolvedValue(mockMovie)
+
+      await createMovie(req, res, next)
+
+      expect(res.status).toHaveBeenCalledWith(201)
+      expect(res.json).toHaveBeenCalledWith(mockMovie)
+      expect(next).not.toHaveBeenCalled()
+    })
+
+    it('should handle errors', async () => {
+      const req = { params, body }
+
+      const mockError = new Error('Database error')
+
+      jest.spyOn(Movie, 'create').mockRejectedValue(mockError)
+
+      await createMovie(req, res, next)
+
+      expect(next).toHaveBeenCalledWith(mockError)
+      expect(res.status).not.toHaveBeenCalled()
+      expect(res.json).not.toHaveBeenCalled()
     })
   })
 })

333fa706a5af6edf3774d9c595b77f72878ccbeb
Christian Olo <christianlois.olo@gmail.com>
2025-09-12 22:33:42 +0800

    feat: add controller for auth with sign up logic

---
+++ src/controllers/auth.js
@@ -0,0 +1,36 @@
+import { compare, hash } from 'bcrypt'
+
+import ApiError from '../services/ApiError.js'
+import { User } from '../models/index.js'
+
+export const login = (req, res, next) => {
+
+}
+
+export const signup = async (req, res, next) => {
+  const { username, password, role } = req.body
+
+  if (!username || !password) {
+    return next(new ApiError('Username and password are required', 400))
+  }
+
+  try {
+
+    const user = await User.findUserByUsername(username)
+
+    if (user) {
+      return next(new ApiError('Username already exists', 409))
+    }
+
+    const saltRounds = parseInt(process.env.BCRYPT_SALT_ROUNDS) || 10
+    const passwordHash = await hash(password, saltRounds)
+    const newUser = await User.create({ username, user_role: role, user_password: passwordHash })
+
+    return res.status(201).json({ user_id: newUser.user_id, username: newUser.username })
+
+  } catch (error){
+
+    next(error)
+
+  }
+}
